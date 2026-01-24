043f04b8ca85a2c17b0d3125008649b4338068da
Greg <grcarette1@gmail.com>
2026-01-24 14:22:57 -0500

    Added usernames to output on api requests

--- api/routes/levels.py
+++ api/routes/levels.py
@@ -7,11 +7,47 @@ router = APIRouter(prefix="/levels", tags=["levels"])
 
 @router.get("/{code}")
 @limiter.limit("30/minute")
+@router.get("/{code}")
+@limiter.limit("30/minute")
 async def get_level(request: Request, code: str):
-    level = await db.levels.find_one({"code": code}, {"_id": 0})
-    if not level:
+    pipeline = [
+        {"$match": {"code": code}},
+        {
+            "$lookup": {
+                "from": "users",
+                "localField": "creators", 
+                "foreignField": "discord_id", 
+                "as": "creator_objects"
+            }
+        },
+        {
+            "$project": {
+                "_id": 0,
+                "name": 1,
+                "code": 1,
+                "imgur_url": 1,
+                "mode": 1,
+                "tournament_legal": 1,
+                "creators": {
+                    "$map": {
+                        "input": "$creator_objects",
+                        "as": "user",
+                        "in": {
+                            "id": "$$user.discord_id",
+                            "username": "$$user.username"
+                        }
+                    }
+                }
+            }
+        }
+    ]
+    
+    results = await db.levels.aggregate(pipeline).to_list(length=1)
+    
+    if not results:
         raise HTTPException(404, "Level not found")
-    return level
+        
+    return results[0]
 
 @router.get("/")
 @limiter.limit("30/minute")
@@ -20,12 +56,40 @@ async def list_levels(
     tournament_legal: bool | None = None,
     mode: str = "party" 
 ):
-    query = {}
-    query["mode"] = mode.lower()
-
-    if tournament_legal is None or tournament_legal is True:
+    query = {"mode": mode.lower()}
+    if tournament_legal is not False: 
         query["tournament_legal"] = True
-    else:
-        pass 
 
-    return await db.levels.find(query, {"_id": 0}).to_list(length=None)
+    pipeline = [
+        {"$match": query},
+        {
+            "$lookup": {
+                "from": "users",
+                "localField": "creators",    
+                "foreignField": "discord_id", 
+                "as": "creator_objects"
+            }
+        },
+        {
+            "$project": {
+                "_id": 0,
+                "name": 1,
+                "code": 1,
+                "imgur_url": 1,
+                "mode": 1,
+                "tournament_legal": 1,
+                "creators": {
+                    "$map": {
+                        "input": "$creator_objects",
+                        "as": "user",
+                        "in": {
+                            "id": "$$user.discord_id",
+                            "username": "$$user.username"
+                        }
+                    }
+                }
+            }
+        }
+    ]
+
+    return await db.levels.aggregate(pipeline).to_list(length=None)
