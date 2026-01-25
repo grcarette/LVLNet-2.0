100d0325e40bd03598e8f237606d80cdfe588dc8
Greg <grcarette1@gmail.com>
2026-01-25 17:44:18 -0500

    Added get_random_levels route

--- api/routes/levels.py
+++ api/routes/levels.py
@@ -49,6 +49,52 @@ async def get_level(request: Request, code: str):
         
     return results[0]
 
+@router.get("/random/{amount}")
+@limiter.limit("10/minute")
+async def get_random_levels(request: Request, amount: int):
+    # 1. Enforce the maximum limit of 5
+    if amount > 5:
+        amount = 5
+    if amount < 1:
+        amount = 1
+
+    pipeline = [
+        # 2. Grab random documents first (most efficient)
+        {"$sample": {"size": amount}},
+        
+        # 3. Perform the same lookup as your other routes
+        {
+            "$lookup": {
+                "from": "users",
+                "localField": "creators", 
+                "foreignField": "discord_id", 
+                "as": "creator_objects"
+            }
+        },
+        {
+            "$project": {
+                "_id": 0,
+                "name": 1,
+                "code": 1,
+                "imgur_url": 1,
+                "mode": 1,
+                "tournament_legal": 1,
+                "creators": {
+                    "$map": {
+                        "input": "$creator_objects",
+                        "as": "user",
+                        "in": {
+                            "id": "$$user.discord_id",
+                            "username": "$$user.username"
+                        }
+                    }
+                }
+            }
+        }
+    ]
+    
+    return await db.levels.aggregate(pipeline).to_list(length=amount)
+
 @router.get("/")
 @limiter.limit("30/minute")
 async def list_levels(
