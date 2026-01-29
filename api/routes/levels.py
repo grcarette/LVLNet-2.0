526401846ce2ee6678308d00b730f3fb7aeb63f8
Greg <grcarette1@gmail.com>
2026-01-29 16:09:02 -0500

    Added route for retrieving multiple levels at once

--- api/routes/levels.py
+++ api/routes/levels.py
@@ -1,14 +1,51 @@
-from fastapi import APIRouter, HTTPException, Request
+from fastapi import APIRouter, HTTPException, Request, Body
 from ..db import db
 from api.models.level import Level
 from api.utils import limiter
+from typing import List
 
 router = APIRouter(prefix="/levels", tags=["levels"])
 
+@router.post("/batch")
+@limiter.limit("60/minute")
+async def get_levels(request: Request, codes: List[str] = Body(...)):
+    pipeline = [
+        {"$match": {"code": {"$in": codes}}},
+        {
+            "$lookup": {
+                "from": "users",
+                "localField": "creators", 
+                "foreignField": "discord_id", 
+                "as": "creator_objects"
+            }
+        },
+        {
+            "$project": {
+                "_id": 0,
+                "name": 1,
+                "code": 1,
+                "imgur_url": 1,
+                "mode": 1,
+                "tournament_legal": 1,
+                "creators": {
+                    "$map": {
+                        "input": "$creator_objects",
+                        "as": "user",
+                        "in": {
+                            "id": "$$user.discord_id",
+                            "username": "$$user.username"
+                        }
+                    }
+                }
+            }
+        }
+    ]
+    results = await db.levels.aggregate(pipeline).to_list(length=len(codes))
+    
+    return results
+
 @router.get("/{code}")
-@limiter.limit("30/minute")
-@router.get("/{code}")
-@limiter.limit("30/minute")
+@limiter.limit("120/minute")
 async def get_level(request: Request, code: str):
     pipeline = [
         {"$match": {"code": code}},
@@ -50,7 +87,7 @@ async def get_level(request: Request, code: str):
     return results[0]
 
 @router.get("/random/{amount}")
-@limiter.limit("30/minute")
+@limiter.limit("120/minute")
 async def get_random_levels(request: Request, amount: int):
     amount = max(1, min(amount, 5))
 
